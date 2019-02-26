b5603345c9827e683a895c3a3b7c75f34038afe0
GilbertHLam <gilberthlam@cmail.carleton.ca>
2019-02-26 10:51:40 -0500

    Can submit highscores

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/views/home/Home';
 import { Route } from 'react-router-dom';
 import Play from './components/views/play/Play';
 import Score from './components/views/score/Score';
+import HighScores from './components/views/highscores/HighScores';
 
 class App extends Component {
 	render() {
@@ -12,6 +13,7 @@ class App extends Component {
 				<Route exact path="/" component={Home} />
 				<Route path="/play" component={Play} />
 				<Route path="/score" component={Score} />
+				<Route path="/highscores" component={HighScores} />
 			</div>
 		);
 	}
